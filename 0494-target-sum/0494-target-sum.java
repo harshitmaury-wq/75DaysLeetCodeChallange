a734749fbbf476e6e2afe08c7a1bba198f2e8aea
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-03-17 11:07:42 +0530

    Time: 16 ms (48.29%), Space: 47 MB (21.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int[][] dp = new int[nums.length][1001];
+        int[][] dp = new int[2001][nums.length];
 
         for(int[] arr : dp) Arrays.fill(arr, Integer.MIN_VALUE);
         return fun(nums, target, 0, 0, dp);
@@ -10,11 +10,11 @@ class Solution {
         if(sum==target && s==nums.length) return 1;
         if(s==nums.length) return 0;
 
-        
+        if(dp[sum+1000][s] != Integer.MIN_VALUE) return dp[sum+1000][s] ;
         int plus = fun(nums, target, sum+nums[s], s+1, dp);
         int minus = fun(nums, target, sum-nums[s], s+1, dp);
 
-        return  plus+minus;
+        return dp[sum+1000][s] = plus+minus;
 
     }
 }
