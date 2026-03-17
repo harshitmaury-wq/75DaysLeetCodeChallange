275080775e5b5b49f1b07fc1fceab6121f0090f8
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-03-17 08:08:30 +0530

    Time: 37 ms (11.15%), Space: 47.4 MB (13.41%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        int[][] dp = new int[coins.length][amount+1];
+        for(int[] arr : dp) Arrays.fill(arr, -1);
+        int ans = fun(coins, 0, amount, dp);
+
+        if(ans ==  Integer.MAX_VALUE) return -1;
+        else return ans;
+    }
+    static int fun (int[] arr, int s, int amt ,int dp[][]){
+        if(amt == 0) return 0;
+        if(amt < 0 ) return Integer.MAX_VALUE;
+        if(s==arr.length) return Integer.MAX_VALUE;
+
+        if(dp[s][amt] != -1) return dp[s][amt];
+        int take = fun(arr, s, amt - arr[s], dp);
+        if(take != Integer.MAX_VALUE) take+=1;
+        int skip = fun(arr, s+1, amt, dp );
+
+        return dp[s][amt] = Math.min(take, skip);
+    }
+}
