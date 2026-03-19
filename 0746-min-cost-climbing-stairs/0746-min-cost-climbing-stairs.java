b6d2b662e8a330aae00fd35ea72bf6fa5d2fe2e5
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-03-19 23:09:05 +0530

    Time: 1 ms (38.27%), Space: 45.1 MB (19.25%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,21 +2,21 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int[] dp = new int[cost.length+1];
         Arrays.fill(dp, -1);
-       return fun(cost, cost.length, dp);
+       return Math.min(fun(cost, 0, dp), fun(cost, 1, dp));
     }
-    static int fun (int[] arr, int e, int[] dp){
+    static int fun (int[] arr, int s, int[] dp){
     
-        if(e<=1) return 0;
+        if(s>=arr.length) return 0;
         
 
 
-        if(dp[e] != -1 ) return dp[e];
-        int left = fun(arr, e-1, dp) + arr[e-1];
+        if(dp[s] != -1 ) return dp[s];
+        int left = fun(arr, s+1, dp) + arr[s];
         
         
-         int right = fun(arr, e-2, dp) + arr[e-2];
+         int right = fun(arr, s+2, dp) + arr[s];
 
-        return dp[e] = Math.min(left, right);
+        return dp[s] = Math.min(left, right);
     }
     
 }
