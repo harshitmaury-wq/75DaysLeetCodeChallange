c03f5c513d30f3dc6d3bd0d015753287bac4376c
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-03-25 22:49:27 +0530

    Time: 5 ms (99.36%), Space: 43.9 MB (56.16%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -10,15 +10,14 @@ class Solution {
 
         if(dp[s] != -1) return dp[s];
         int max = Integer.MIN_VALUE;
+        int runmax = Integer.MIN_VALUE;
         for(int i=1; i<=k && s + i <= arr.length; i++){
-            int sum = i*mx(arr, s, s+i-1) + fun(arr, k, s+i, dp);
+            runmax = Math.max(arr[s+i-1], runmax);
+            int sum = i*runmax + fun(arr, k, s+i, dp);
             max = Math.max(max, sum);
         }
         return dp[s] = max;
     }
-
-
-
 
     static int mx(int[] arr, int a , int b){
         int mx = arr[a];
