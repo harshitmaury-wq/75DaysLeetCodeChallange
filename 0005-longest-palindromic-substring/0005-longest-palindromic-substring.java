7645e090b1444c6dbbef5eea1764136d84827df6
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-03-27 13:21:37 +0530

    Time: 269 ms (16.62%), Space: 105.6 MB (5.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,42 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int I=0;
-        int J=0;
-        int max=0;
-        for(int i=0; i<s.length();i++){
-            for(int j=s.length()-1; j>=i; j--){
-                if(s.charAt(i)==s.charAt(j)){
-                    if(pali(s,i,j)){
-                        if(j-i+1>max){
-                            max=j-i+1;
-                            I=i; J=j;
-                        }
+        int[][] dp = new int[s.length()][s.length()];
+        for(int[] ele: dp) Arrays.fill(ele, -1);
+        int max = Integer.MIN_VALUE;
+        int I=0, J=0;
+        for(int i=0; i<s.length(); i++){
+            for(int j=i; j<s.length(); j++){
+                if(ispalin(s, i, j, dp)){
+                    if(j-i+1 > max){
+                        max = j-i+1;
+                        I=i;
+                        J=j;
                     }
+
                 }
+                
             }
         }
-        StringBuilder sb=new StringBuilder("");
-        for(int i=I; i<=J; i++) sb.append(s.charAt(i));
-        return sb.toString();
+        return s.substring(I, J+1);
     }
-    static boolean pali(String s, int a, int b){
-        int i=a,j=b;
-        while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;j--;
+    static boolean ispalin(String s, int a, int b, int[][] dp){
+        if(a>b) return true;
+
+
+        if(dp[a][b] != -1) {
+            if(dp[a][b] ==1) return true;
+            return false;
         }
-        return true;
+        if(s.charAt(a) != s.charAt(b))  { dp[a][b] = 0; return false ;}
+
+        if(s.charAt(a)==s.charAt(b)) {
+            boolean b1 = ispalin(s, a+1, b-1, dp);
+            if(b1) dp[a][b] = 1;
+            else dp[a][b] = 0;
+            return b1;
+        }
+         return false;
+    
+
     }
 }
