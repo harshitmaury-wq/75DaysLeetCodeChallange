f7a5d8ca1475f9ee38e8b035fe386d54bd255f33
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-03-27 12:16:36 +0530

    Time: 1612 ms (5.01%), Space: 42.8 MB (77.54%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,33 +1,28 @@
 class Solution {
     public int countSubstrings(String s) {
 
-        int[][] dp = new int[s.length()][s.length()];
-        for(int[] ele: dp) Arrays.fill(ele, -1);
+        
+       
         
         int count = 0;
         for(int i=0; i<s.length(); i++){
             for(int j=i; j<s.length(); j++){
 
-                if(ispalin(s, i, j, dp) ) count++ ;
+                if(ispalin(s, i, j) ) count++ ;
             }
         }
         return count;
 
     }
-    static boolean ispalin(String s, int a, int b, int[][] dp){
+    static boolean ispalin(String s, int a, int b){
         if(a>b) return true;
 
 
-        if(dp[a][b] != -1) {
-            if(dp[a][b] ==1) return true;
-            return false;
-        }
-        if(s.charAt(a) != s.charAt(b))  { dp[a][b] = 0; return false ;}
+        if(s.charAt(a) != s.charAt(b))  {  return false ;}
 
         if(s.charAt(a)==s.charAt(b)) {
-            boolean b1 = ispalin(s, a+1, b-1, dp);
-            if(b1) dp[a][b] = 1;
-            else dp[a][b] = 0;
+            boolean b1 = ispalin(s, a+1, b-1);
+           
             return b1;
         }
          return false;
