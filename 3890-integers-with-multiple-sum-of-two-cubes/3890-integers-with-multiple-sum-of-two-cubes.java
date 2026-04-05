d0b316f7df6be5628670950ebb9522f7ce58773b
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-04-05 19:27:06 +0530

    Time: 350 ms (26.06%), Space: 154.5 MB (91.29%) - LeetHub

---
+++ 3890-integers-with-multiple-sum-of-two-cubes/3890-integers-with-multiple-sum-of-two-cubes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findGoodIntegers(int n) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int N = (int)Math.cbrt(n);
+       
+        for(int i=1; i<=N; i++){
+            for(int j=i ; j<=N; j++){
+                int x = (i*i*i) + (j*j*j) ;
+                if(x > n) continue ;
+                if(hm.containsKey(x)) hm.put(x, hm.get(x)+1);
+                else hm.put(x, 1) ;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(Integer x : hm.keySet()){
+            if(hm.get(x) >= 2) list.add(x);
+        }
+        Collections.sort(list);
+        return list;
+    }   
+}
