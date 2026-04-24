fba78d81ebd2256c3960a9ee0b06896d09bbf543
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-04-24 20:10:56 +0530

    Time: 33 ms (65.08%), Space: 61.9 MB (75.66%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,46 @@
+class Trie {
+
+    class Node {
+        Node[] children = new Node[26];
+        boolean isEnd = false;
+    }
+
+    private Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node curr = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Node node = findNode(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return findNode(prefix) != null;
+    }
+
+    private Node findNode(String str) {
+        Node curr = root;
+        for (char c : str.toCharArray()) {
+            int idx = c - 'a';
+            if (curr.children[idx] == null) {
+                return null;
+            }
+            curr = curr.children[idx];
+        }
+        return curr;
+    }
+}
