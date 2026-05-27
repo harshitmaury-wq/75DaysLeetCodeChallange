ba53442ea6ec06b9fe835041298724dad9118437
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-05-27 09:39:22 +0530

    Time: 24 ms (5.48%), Space: 51.4 MB (99.76%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -8,7 +8,7 @@ class Solution {
 
         for(int i=0; i<trust.length; i++){
             list.get(trust[i][0]).add(trust[i][1]);
-            if(hm.containsKey(trust[i][1])) hm.put(trust[i][1], hm.get(trust[i][1])+1) ;
+            if(hm.containsKey(trust[i][1])) hm.put(trust[i][1], hm.get(trust[i][1]) +1) ;
             else hm.put(trust[i][1], 1) ;
         }
         for(int i=1; i<n+1; i++){
