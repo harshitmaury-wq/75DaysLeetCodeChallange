8c864e528bd930aadf64bf29203527c602e91832
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-05-31 18:31:17 +0530

    Time: 26 ms (81.52%), Space: 107.5 MB (47.81%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        Stack<Long> st = new Stack<>() ;
+        long a = (long)mass ;
 
         Arrays.sort(asteroids) ;
         if(asteroids[0] > mass) return false ;
 
-        st.push((long)mass) ;
+        
         int i = 0;
-        while(i<asteroids.length && st.peek() >= asteroids[i]){
-            long a = st.pop();
-            st.push(a + asteroids[i]) ;
+        while(i<asteroids.length && a >= asteroids[i]){
+            a += asteroids[i];
             i++ ;
         }
         if(i==asteroids.length) return true;
