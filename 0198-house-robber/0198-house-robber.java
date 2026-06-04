05910f0575fe083cd42e51e75211ed7b9f21f71f
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-06-04 11:54:36 +0530

    Time: 0 ms (100%), Space: 42.9 MB (36.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,7 +5,7 @@ class Solution {
        
         for(int i=0; i<nums.length; i++){
             int take = nums[i] ;
-            if(i-2 >=0) take += dp[i-2];
+            if(i-2 >= 0) take += dp[i-2];
             int skip = 0;
             if(i-1>=0) skip += dp[i-1];
             dp[i] = Math.max(take, skip);
