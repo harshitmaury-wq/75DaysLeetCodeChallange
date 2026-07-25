a2434e5612861646a5fd59d744eaf9fba7bdff84
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-07-25 17:02:35 +0530

    Time: 0 ms (100%), Space: 42.4 MB (33.71%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx > sx && ty > sy) {
+            if(tx > ty) tx = tx%ty;
+            else ty%=tx ;
+        }
+        if(tx == sx && ty >= sy) {
+            return (ty - sy) % tx == 0 ;
+        }
+        else if (ty == sy && tx >= sx) {
+            return (tx - sx) % ty == 0 ;
+        }
+        return false ;
+    }
+}
