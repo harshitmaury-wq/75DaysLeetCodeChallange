e4ec158963f693fb1c02cba1903d3e595c6341dd
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-07-29 19:50:29 +0530

    Time: 2 ms (90.04%), Space: 44.1 MB (72.68%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,61 +1,39 @@
 class Solution {
- 
-    class Node{
-        int x;
-        int y;
-        Node(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<Node> q = new LinkedList<>() ;
-
+        int m = grid.length ;
+        int n = grid[0].length ;
+        int[][] vis = new int[m][n] ;
+        Queue<int[]> q = new LinkedList<>() ;
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j< n; j++) {
+                if(grid[i][j] == 2) 
+                {q.add(new int[]{i, j}) ; vis[i][j] = 1 ;}
+            }
+        }
         int time = -1;
-        int fresh = 0;
-        
-        for(int i=0; i<m ; i++){
-            for(int j=0; j<n ; j++){
-                if(grid[i][j] == 2 ) { q.add(new Node(i, j)); } 
-                if(grid[i][j] == 1) fresh++;
-            }
-        }
-
-        if(fresh == 0) return 0;
-
-        while(!q.isEmpty()) {
-            int size = q.size() ;
-            while(size != 0){
-                Node temp = q.remove() ;
-                size--;
-                if(temp.y+1 < n && grid[temp.x][temp.y+1]==1 ) {
-                    grid[temp.x][temp.y+1]=2;
-                    fresh--;
-                    q.add(new Node(temp.x, temp.y+1)) ;
-                }
-                if(temp.y-1 >= 0 && grid[temp.x][temp.y-1]==1 ) {
-                    grid[temp.x][temp.y-1]=2;
-                    fresh--;
-                    q.add(new Node(temp.x, temp.y-1)) ;
-                }
-                if(temp.x+1 < m && grid[temp.x+1][temp.y]==1 ) {
-                    grid[temp.x+1][temp.y]=2;
-                    fresh--;
-                    q.add(new Node(temp.x+1, temp.y)) ;
-                }
-                if(temp.x-1 >= 0 && grid[temp.x-1][temp.y]==1 ) {
-                    grid[temp.x-1][temp.y]=2;
-                    fresh--;
-                    q.add(new Node(temp.x-1, temp.y)) ;
-                }
-            }
-            time++;
-        }
        
-       if(fresh == 0) return time;
-       return -1;
-        
+        while(!q.isEmpty()) {
+            time++ ;
+            int size = q.size() ;
+            for(int i = 1; i<=size; i++) {
+            int[] t = q.remove() ;
+            int x = t[0] ;
+            int y = t[1] ; 
+            
+            
+            if(x + 1 < m && vis[x+1][y]==0 && grid[x+1][y]==1) {vis[x+1][y] = 1;q.add(new int[]{x+1, y}) ;}
+            if(x - 1 >= 0 && vis[x-1][y]==0 && grid[x-1][y]==1) {vis[x-1][y] = 1;q.add(new int[]{x-1, y}) ;}
+            if(y + 1 < n && vis[x][y+1]==0 && grid[x][y+1]==1) {vis[x][y+1] = 1;q.add(new int[]{x, y+1}) ;}
+            if(y - 1 >= 0  && vis[x][y-1]==0 && grid[x][y-1]==1) {vis[x][y-1] = 1;q.add(new int[]{x, y-1}) ;}
+            }
+        }
+
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j< n; j++){
+                if(vis[i][j] == 0 && grid[i][j] == 1) return  -1 ;
+            }
+        }
+        if(time == -1) return 0 ;
+        return time ;
     }
 }
