75eaaccbe1627c14249d9e5bc958e126be00665f
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-07-29 23:32:12 +0530

    Time: 4 ms (15.37%), Space: 47.8 MB (38.87%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,58 +1,35 @@
 class Solution {
-     class Node{
-            int r;
-            int c;
-            Node(int x, int y){
-                this.r = x;
-                this.c = y;
-            }
-        }
     public void solve(char[][] board) {
-       
         int m = board.length;
-        int n= board[0].length;
+        int n = board[0].length ;
+        int[][] vis = new int[m][n] ;
 
-        int[][] visi = new int[m][n];
+        Queue<int[]> q = new LinkedList<>() ;
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if((i==0 || j==0 || i==m-1 || j==n-1) && board[i][j]=='O' && visi[i][j]==0){
-                dfs(visi, new Node(i, j), board, m, n);
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if((i == 0 || i==m-1 || j == 0|| j==n-1 ) && vis[i][j]==0 && board[i][j]=='O'){
+                    q.add(new int[]{i, j}) ;
+                    vis[i][j] = 1;
                 }
             }
         }
+        while(!q.isEmpty()) {
+            int[] t = q.remove() ;
+            int x = t[0] ;
+            int y = t[1] ;
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(visi[i][j]==0) board[i][j] = 'X';
+            if(x + 1 < m && vis[x+1][y] == 0 && board[x+1][y] == 'O' ) {q.add(new int[]{x+1, y} ); vis[x+1][y] = 1 ;} 
+            if(x - 1 >= 0 && vis[x-1][y] == 0 && board[x-1][y] == 'O') {q.add(new int[]{x-1, y} ); vis[x-1][y] = 1 ;} 
+            if(y + 1 < n && vis[x][y+1] == 0 && board[x][y+1] == 'O') {q.add(new int[]{x, y+1} ); vis[x][y+1] = 1 ;} 
+            if(y - 1 >= 0 && vis[x][y-1] == 0 && board[x][y-1] == 'O') {q.add(new int[]{x, y-1} ); vis[x][y-1] = 1 ;} 
+        }
+
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(vis[i][j] == 0 ) board[i][j] = 'X' ;
             }
         }
         
-       
-        
-    }
-    void dfs(int[][] visi, Node s, char[][]b, int m, int n){
-        visi[s.r][s.c] = 1;
-
-        if(s.r+1 <m && visi[s.r+1][s.c]==0 && b[s.r+1][s.c]=='O'){
-                dfs(visi, new Node(s.r+1, s.c),b, m, n);
-                
-            }
-            
-            if(s.r-1 >=0 && visi[s.r-1][s.c]==0 && b[s.r-1][s.c]=='O'){
-                dfs(visi, new Node(s.r-1, s.c),b, m, n);
-                
-            }
-            
-            if(s.c+1 <n && visi[s.r][s.c+1]==0 && b[s.r][s.c+1]=='O'){
-                dfs(visi, new Node(s.r, s.c+1),b, m, n);
-              
-            }
-            
-            if(s.c-1 >=0 && visi[s.r][s.c-1]==0 && b[s.r][s.c-1]=='O'){
-                dfs(visi, new Node(s.r, s.c-1),b, m, n);
-              
-            }
-
     }
 }
