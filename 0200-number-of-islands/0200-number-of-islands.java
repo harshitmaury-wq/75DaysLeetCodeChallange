c07dd1aa6c10bc2d972267f0c3f8189a6d4f1eea
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-07-30 00:15:01 +0530

    Time: 4 ms (46.24%), Space: 51.7 MB (81.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,48 +1,31 @@
 class Solution {
-    class Node{
-        int x;
-        int y;
-        Node(int x, int y) {
-           this.x = x;
-           this.y = y ;
-        }
-    }
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] visi = new int[m][n];
+        int m = grid.length ;
+        int n = grid[0].length ;
 
-        int count = 0;
-        for(int i=0; i< m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == '1' && visi[i][j] == 0) {
-                count++;
-                bfs(visi, grid, new Node(i, j), m , n) ;
+        int[][] vis = new int[m][n] ;
+
+        int c = 0; 
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == '1' && vis[i][j] == 0){
+                    c++;
+                    dfs(grid, i, j, vis) ;
                 }
             }
         }
-        return count ;
-    }
-     void bfs(int[][] visi, char[][] grid, Node s, int m, int n){
-        Queue<Node> q = new LinkedList<>() ;
-        q.add(s);
-        visi[s.x][s.y]  = 1 ;
 
-        while(!q.isEmpty()) {
-            Node temp = q.remove() ;
-            if(temp.y+1 < n && visi[temp.x][temp.y+1] == 0 && grid[temp.x][temp.y+1] == '1') {
-                q.add(new Node(temp.x,temp.y+1)) ; 
-                visi[temp.x][temp.y+1] = 1; }
-            if(temp.y-1 >= 0 && visi[temp.x][temp.y-1] == 0  && grid[temp.x][temp.y-1] == '1') {
-                q.add(new Node(temp.x,temp.y-1)) ; 
-                visi[temp.x][temp.y-1] = 1; }
-            if(temp.x+1 < m && visi[temp.x+1][temp.y] == 0 && grid[temp.x+1][temp.y] == '1') {
-                q.add(new Node(temp.x+1,temp.y)) ; 
-                visi[temp.x+1][temp.y] = 1; }
-            if(temp.x-1 >= 0 && visi[temp.x-1][temp.y] == 0 && grid[temp.x-1][temp.y] == '1') {
-                q.add(new Node(temp.x-1,temp.y)) ; 
-                visi[temp.x-1][temp.y] = 1; }
-           
-        }
+        return c ;
+    }
+    void dfs (char[][] grid, int x, int y, int[][] vis) {
+        if(x == grid.length || x < 0 || y==grid[0].length || y<0 || vis[x][y] == 1 || grid[x][y] == '0') return ;
+
+        vis[x][y] = 1;
+        
+        dfs(grid, x+1, y, vis) ;
+        dfs(grid, x-1, y, vis) ;
+        dfs(grid, x, y+1, vis) ;
+        dfs(grid, x, y-1, vis) ;
+
     }
 }
