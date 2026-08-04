ba394a01522821ba2988b8392e6e8e204b30cb54
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-08-04 11:20:22 +0530

    Time: 274 ms (5.2%), Space: 100.2 MB (95.95%) - LeetHub

---
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minAbsoluteDifference(List<Integer> nums, int x) {
+        int min = Integer.MAX_VALUE ;
+        TreeMap<Integer, Integer> tm  = new TreeMap<>() ;
+
+        for(int i = x; i<nums.size(); i++) {
+            if(tm.containsKey(nums.get(i))) tm.put(nums.get(i), tm.get(nums.get(i)) + 1 ) ;
+            else tm.put(nums.get(i) , 1) ;
+        }
+        int j = x;
+        int i = 0 ;
+
+        while(j < nums.size()) {
+            int justgreater, justsmaller ;
+            if(tm.ceilingKey(nums.get(i)) != null) justgreater = tm.ceilingKey(nums.get(i)) - nums.get(i);
+            else justgreater = Integer.MAX_VALUE ;
+            if(tm.floorKey(nums.get(i)) != null ) justsmaller = nums.get(i) - tm.floorKey(nums.get(i))  ;
+            else justsmaller = Integer.MAX_VALUE ;
+
+            min = Math.min(min, Math.min(justgreater, justsmaller)) ;
+
+            tm.put(nums.get(j), tm.get(nums.get(j)) - 1) ;
+            if(tm.get(nums.get(j)) == 0) tm.remove(nums.get(j)) ;
+
+            i++; j++ ;
+        }
+        return min ;
+    }
+}
