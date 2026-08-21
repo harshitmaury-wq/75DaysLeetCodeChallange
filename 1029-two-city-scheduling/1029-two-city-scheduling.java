277250557a20f2df94dfe6132ec03fba9bc129ef
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-08-21 12:33:38 +0530

    Time: 7 ms (5.98%), Space: 47.9 MB (5.3%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -11,7 +11,6 @@ class Solution {
     }
     int fun(int[][] costs, int s, int ano, int bno, int[][][] dp) {
         if(s == costs.length || (ano == 0 && bno == 0)) return 0;
-        if(dp[s][ano][bno] != -1) return dp[s][ano][bno] ;
         if(ano == 0) {
             int sum = 0;
             for(int i = s; i<costs.length; i++) {
@@ -27,7 +26,7 @@ class Solution {
             return dp[s][ano][bno] = sum ;
         }
 
-        
+        if(dp[s][ano][bno] != -1) return dp[s][ano][bno] ;
         return dp[s][ano][bno] = Math.min(costs[s][0] + fun(costs, s+1, ano-1, bno, dp), costs[s][1] + fun(costs, s+1, ano, bno-1, dp)) ;
     }
 }
