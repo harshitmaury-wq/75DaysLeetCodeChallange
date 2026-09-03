4be0a0a2d7bbb6dd5febab85c13e390416577f55
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-09-03 18:37:15 +0530

    Time: 14 ms (54.8%), Space: 46.7 MB (18.59%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxConsecutiveAnswers(String s, int k) {
+        int j = 0; 
+        int i = 0;
+        int maxlen = 0;
+        
+        int maxoccur = 0;
+        int[] arr = new int[26] ;
+
+        while(j < s.length()) {
+            arr[s.charAt(j) - 'A'] ++;
+
+            maxoccur = Math.max(maxoccur, arr[s.charAt(j) - 'A']) ;
+
+            int window = j - i + 1 ;
+
+            while(i < s.length() && window - maxoccur > k) {
+                arr[s.charAt(i) - 'A'] -- ;
+                i++ ;
+
+                for(int x : arr) maxoccur = Math.max(maxoccur, x) ;
+
+                window = j - i + 1 ;
+            }
+            maxlen = Math.max(maxlen, window) ;
+            j++ ;
+        }
+
+        return maxlen ;
+    }
+}
