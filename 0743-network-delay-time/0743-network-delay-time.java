0ad76a99bc34797ae8995cec4e34c7192168bb52
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-09-11 18:49:52 +0530

    Time: 14 ms (31.23%), Space: 48.9 MB (66.73%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,44 +1,48 @@
 class Solution {
-    class Node{
-        int val;
-        int w;
-        Node(int val, int w){
-            this.val = val;
-            this.w = w;
-        }
-    }
     public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<Node>> list = new ArrayList<>() ;
-        for(int i=0; i<n+1; i++) list.add(new ArrayList<>());
+        List<List<int[]>> list = new ArrayList<>() ;
 
-        for(int i=0; i<times.length; i++){
-            list.get(times[i][0]).add(new Node(times[i][1], times[i][2])) ;
+        for(int i = 0; i<n+1 ; i++) {
+            list.add(new ArrayList<>()) ;
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> { return a.w - b.w ; }) ;
-        int[] dis = new int[n+1] ;
-        Arrays.fill(dis, Integer.MAX_VALUE) ;
+        for(int i = 0; i<times.length ;i++) {
+            list.get(times[i][0]).add(new int[]{times[i][1], times[i][2]}) ;
+        }
 
-        dis[k]  = 0;
-        pq.add(new Node(k, 0));
+        int[] dis= new int[n+1] ;
+        Arrays.fill(dis, Integer.MAX_VALUE ) ;
+        dis[k] = 0 ;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {return a[0]-b[0];}) ;
+        pq.add(new int[]{0,k}) ;
 
         while(!pq.isEmpty()) {
-            Node temp = pq.remove() ;
-            int val = temp.val;
-            int w = temp.w ;
+            int[] temp = pq.remove() ;
+            int d = temp[0] ;
+            int node = temp[1] ;
 
-            for(int i=0; i<list.get(val).size(); i++){
-                if(w + list.get(val).get(i).w < dis[list.get(val).get(i).val]){
-                    dis[list.get(val).get(i).val] = w + list.get(val).get(i).w ;
-                    pq.add(new Node(list.get(val).get(i).val, dis[list.get(val).get(i).val]));
+            if(dis[node] < d) continue ;
+
+            for(int i= 0 ; i<list.get(node).size(); i++) {
+                int next = list.get(node).get(i)[0] ;
+                int wei = list.get(node).get(i)[1] ;
+                
+                if(d + wei < dis[next]) {
+                    dis[next] = d + wei;
+                    pq.add(new int[]{dis[next] , next}) ;
                 }
             }
-        } 
-        int max = Integer.MIN_VALUE;
-        for(int i=1; i<dis.length; i++){
-            if(dis[i] == Integer.MAX_VALUE) return -1;
-            max = Math.max(dis[i], max) ;
         }
+
+        int max = Integer.MIN_VALUE ;
+
+        for(int i = 1; i<dis.length; i++) {
+            if(dis[i] == Integer.MAX_VALUE) return -1;
+
+            max = Math.max(max, dis[i]) ;
+        }
+
         return max;
     }
 }
