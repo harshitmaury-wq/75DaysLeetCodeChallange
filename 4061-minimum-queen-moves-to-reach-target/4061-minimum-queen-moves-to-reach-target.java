8def07c7b4d8a30620234a2c1788ca11913d1920
harshitmaury-wq <harshitmaurya761703@gmail.com>
2026-09-26 22:25:59 +0530

    Time: 1 ms (95.47%), Space: 44.1 MB (66.57%) - LeetHub

---
+++ 4061-minimum-queen-moves-to-reach-target/4061-minimum-queen-moves-to-reach-target.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minQueenMoves(int[] s, int[] t) {
+        if(s[0]==t[0] && s[1] == t[1]) return 0; 
+        if(s[0] == t[0] || s[1] == t[1]) return 1 ;
+        if(Math.abs(s[0]-t[0]) == Math.abs(s[1]-t[1])) return 1 ;
+
+        return 2 ;
+
+    }
+}
